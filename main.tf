1592a244bb0f7ed953cb770d01f8e4fc5eb070c4
ecuanate <nathan.halter93@gmail.com>
2025-02-26 14:18:55 +0000

    test

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
-  key_name = aws_key_pair.key_pair_terraform.key_name
+  key_name = var.key_name
   tags = {
     Name = "terraform_instance"
   }
